42b33bf4fed1494558ac340964a6fdf7febcfc79
Krish Meghapara <159990261+KrishMeghapara@users.noreply.github.com>
2025-07-06 16:45:41 +0530

    Delete Server directory
    old code

--- Server/models/Users.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const UserSchema = new mongoose.Schema({
-    _id: String,
-    url: String
-})
-
-const UserModel = mongoose.model("users",UserSchema)
-module.exports = UserModel
